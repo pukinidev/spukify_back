8b932a5e9a735c112aa89c2ef0a56185cb52f505
PUKINIDEV <v.caro@uniandes.edu.co>
2024-05-31 19:50:48 -0500

    new database

--- app/config/database.py
+++ app/config/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./spukify.db"
+POSTGRESQL_USER = os.getenv("POSTGRESQL_USER")
+POSTGRESQL_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
+POSTGRESQL_HOST = os.getenv("POSTGRESQL_HOST")
+
+SQLALCHEMY_DATABASE_URL = f'postgres://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}:5432/postgres'
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
--- app/main.py
+++ app/main.py
@@ -5,26 +5,13 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
-
-# Routes
 from routers import user_router, track_router, playlist_router, album_router, artist_router, category_router
-
 
 app = FastAPI(
     title="Spukify Backend",
     description="This is the backend for the Spukify project",
 )
 
-"""
-    Routers of the application
-    List of routers included:
-    - track_router
-    - playlist_router
-    - album_router
-    - artist_router
-    - category_router
-    - user_router
-"""
 app.include_router(user_router.router)
 app.include_router(track_router.router)
 app.include_router(playlist_router.router)
@@ -44,6 +31,5 @@ app.add_middleware(
 def main_function():
     return RedirectResponse(url="/docs/")
 
-# Run the server
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8080, reload=True)
