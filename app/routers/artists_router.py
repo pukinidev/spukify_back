bbef84b0547b9d78ff280cea19eb4c31562b758a
PUKINIDEV <v.caro@uniandes.edu.co>
2024-06-03 13:10:45 -0500

    fix on artists!

--- app/routers/artists_router.py
+++ app/routers/artists_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from controllers.artist_controller import create, delete_by_id, get_all, get_by_id
 from config.database import get_db
 from sqlalchemy.orm import Session
@@ -11,6 +11,8 @@ artists_router = APIRouter(
 
 @artists_router.post("/create", tags=['Artists'], response_model=Artist)
 def create_artist(artist: Artist, db: Session = Depends(get_db)):
+    if artist:
+        raise HTTPException(status_code=400, detail=f"Artist with id {artist.id} already exists!")
     return create(db, artist)
 
 @artists_router.get("/", tags=['Artists'], response_model=list[Artist])
@@ -18,9 +20,9 @@ def get_all_artists(db: Session = Depends(get_db)):
     return get_all(db)
 
 @artists_router.get("/{artist_id}", tags=['Artists'], response_model=Artist)
-def get_artist_by_id(artist_id: int, db: Session = Depends(get_db)):
+def get_artist_by_id(artist_id: str, db: Session = Depends(get_db)):
     return get_by_id(db, artist_id)
 
 @artists_router.delete("/{artist_id}", tags=['Artists'])
-def delete_artist_by_id(artist_id: int, db: Session = Depends(get_db)):
+def delete_artist_by_id(artist_id: str, db: Session = Depends(get_db)):
     return delete_by_id(db, artist_id)
