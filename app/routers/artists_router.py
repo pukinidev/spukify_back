bfaa455ec9d4bcdff68fbca582b1087464f363ff
pukinidev <v.caro@uniandes.edu.co>
2024-10-19 20:14:24 -0500

    new artist route

--- app/routers/artists_router.py
+++ app/routers/artists_router.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from controllers.artist_controller import create, delete_by_id, get_all, get_by_id
 from config.database import get_db
@@ -9,13 +10,20 @@ artists_router = APIRouter(
     tags=['Artists'],
 )
 
+@artists_router.get("/", tags=['Artists'], response_model=list[Artist])
+def get_all_artists(db: Session = Depends(get_db)):
+    return get_all(db)
+
+
 @artists_router.post("/create", tags=['Artists'], response_model=Artist)
 def create_artist(artist: Artist, db: Session = Depends(get_db)):
     return create(db, artist)
 
-@artists_router.get("/", tags=['Artists'], response_model=list[Artist])
-def get_all_artists(db: Session = Depends(get_db)):
-    return get_all(db)
+@artists_router.post("/createartists", tags=['Artists'], response_model=List[Artist])
+def create_artists(artists: List[Artist], db: Session = Depends(get_db)):
+    for artist in artists:
+        create(db, artist)
+    return artists
 
 @artists_router.get("/{artist_id}", tags=['Artists'], response_model=Artist)
 def get_artist_by_id(artist_id: str, db: Session = Depends(get_db)):
