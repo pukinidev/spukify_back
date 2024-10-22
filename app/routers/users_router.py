8ce47f38ea4e3e0b6c290fbb6a9dc57be637fd1a
pukinidev <v.caro@uniandes.edu.co>
2024-10-22 08:28:22 -0500

    refactor: order of functions

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -15,16 +15,16 @@ users_router = APIRouter(
 def get_all_users(db: Session = Depends(get_db)):
     return get_all(db)
 
-@users_router.post("/createusers", tags=['Users'], response_model=list[User])
-def create_users(users: list[User], db: Session = Depends(get_db)):
-    for user in users:
-        create(db, user)
-    return users
 
 @users_router.post("/create", tags=['Users'] ,response_model=User)
 def create_user(user: User, db: Session = Depends(get_db)):
     return create(db, user)
 
+@users_router.post("/createusers", tags=['Users'], response_model=list[User])
+def create_users(users: list[User], db: Session = Depends(get_db)):
+    for user in users:
+        create(db, user)
+    return users
 
 
 @users_router.get("/{user_id}",tags=['Users'], response_model=User)
